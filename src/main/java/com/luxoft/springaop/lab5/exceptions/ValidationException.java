6fbf5866baa3aed97ea9af2d05e535451b446fd7
Andrey Trusov <trusov@haulmont.com>
2019-10-25 14:58:13 +0400

    hw 5

---
+++ src/main/java/com/luxoft/springaop/lab5/exceptions/ValidationException.java
@@ -0,0 +1,11 @@
+package com.luxoft.springaop.lab5.exceptions;
+
+//import java.util.logging.Logger;
+
+public class ValidationException extends RuntimeException {
+//    private final static Logger LOG = Logger.getLogger(ValidationException.class.getName());
+
+    public ValidationException(String message) {
+//        LOG.info("ValidationException: " + message);
+    }
+}
